ae96b1edf58e3f55a61b2bba26fd2421ddf5f600
German H Flores <germanhflores@Germans-MacBook-Pro.local>
2013-05-28 00:40:54 -0700

    Initial Commit

---
+++ iNSPro1.0/CMGDSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  CMGDSecondViewController.h
+//  iNSPro1.0
+//
+//  Created by German H Flores on 5/28/13.
+//  Copyright (c) 2013 German H Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMGDSecondViewController : UIViewController
+
+@end
---
+++ iNSPro1.0/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iNSPro1.0
+//
+//  Created by German H Flores on 5/28/13.
+//  Copyright (c) 2013 German H Flores. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CMGDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CMGDAppDelegate class]));
+    }
+}
